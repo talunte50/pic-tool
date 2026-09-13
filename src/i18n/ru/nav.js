0130789c7c349a8d3f3002bab913a417d0f4017a
talunte50 <talunte50@outlook.com>
2026-09-13 12:53:40 +0800

    chore: import src/i18n/ru/nav.js

---
+++ src/i18n/ru/nav.js
@@ -0,0 +1,46 @@
+export default {
+    editor: {
+        name: 'Редактор',
+        title: 'Фоторедактор',
+    },
+    beautifier: {
+        name: 'Оформитель',
+        title: 'Оформитель скриншотов',
+    },
+    rounded: {
+        name: 'Закругленный',
+        title: 'Фото в закругленную форму',
+    },
+    remover: {
+        name: 'Удалитель',
+        title: 'Удалитель фона фотографии',
+    },
+    blurBackground: {
+        name: 'Размытие',
+        title: 'Размыть фон онлайн',
+    },
+    compressor: {
+        name: 'Компрессор',
+        title: 'Компрессор изображений',
+    },
+    longImage: {
+        name: 'Длинное',
+        title: 'Склейка длинных изображений',
+    },
+    videoConvert: {
+        name: 'Video',
+        title: 'Video Convert',
+    },
+    convert: {
+        name: 'Convert',
+        title: 'Image Convert'
+    },
+    viewer: {
+        name: 'Viewer',
+        title: 'Office Viewer'
+    },
+    screenshot: {
+        name: 'скриншот',
+        title: 'сделать скриншот'
+    }
+};
