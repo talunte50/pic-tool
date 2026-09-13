51af6ea978926c56179d3ebf6c1b55112dd0c880
talunte50 <talunte50@outlook.com>
2026-09-13 12:53:29 +0800

    chore: import src/i18n/pt-br/nav.js

---
+++ src/i18n/pt-br/nav.js
@@ -0,0 +1,46 @@
+export default {
+    editor: {
+        name: 'Editor',
+        title: 'Editor de fotos'
+    },
+    beautifier: {
+        name: 'Embelezar',
+        title: 'Embelezador de captura de tela'
+    },
+    rounded: {
+        name: 'Bordas',
+        title: 'Foto com cantos arredondados'
+    },
+    remover: {
+        name: 'Remover',
+        title: 'Removedor de fundo de foto'
+    },
+    blurBackground: {
+        name: 'Desfoque',
+        title: 'Desfocar fundo online',
+    },
+    compressor: {
+        name: 'Compressor',
+        title: 'Compressor de imagens'
+    },
+    longImage: {
+        name: 'Imagem longa',
+        title: 'Compositor de imagem longa'
+    },
+    videoConvert: {
+        name: 'Vídeo',
+        title: 'Conversor de vídeo'
+    },
+    convert: {
+        name: 'Converter',
+        title: 'Conversor de imagens'
+    },
+    viewer: {
+        name: 'Visualizar',
+        title: 'Visualizador de Office'
+    },
+    screenshot: {
+        name: 'Captura',
+        title: 'Fazer captura de tela'
+    }
+}
