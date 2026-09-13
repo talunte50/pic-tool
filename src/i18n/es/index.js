4425c402fa2396027d610b16e8ca6652a7df4421
talunte50 <talunte50@outlook.com>
2026-09-13 12:52:04 +0800

    chore: import src/i18n/es/index.js

---
+++ src/i18n/es/index.js
@@ -0,0 +1,35 @@
+import nav from './nav';
+import editor from './editor';
+import beautifier from './beautifier';
+import rounded from './rounded';
+import remover from './remover';
+import blurBackground from './blurBackground';
+import compressor from './compressor';
+import longImage from './longImage';
+import screenshot from './screenshot';
+import videoConvert from './videoConvert';
+import convert from './convert';
+import viewer from './viewer';
+
+export default {
+    title: 'Editor de fotos y capturas online gratis | pic-tool',
+    description:
+        'Edita fotos en línea de forma gratuita, redimensiona y filtra cualquier foto, edita fotos en el navegador, convierte imágenes a jpg/png/jpeg/webp, captura fácilmente áreas o páginas completas',
+    keywords:
+        'Shot Easy, easy shot, captura de pantalla, editar foto, convertidor de fotos, convertidor de imágenes, editor en línea, cambiar formato de imagen en línea, convertir imagen a jpg, jpg a webp, jpg a png',
+    privacy: 'Privacidad',
+    terms: 'Términos',
+    blog: 'Blog',
+    nav,
+    editor,
+    beautifier,
+    rounded,
+    remover,
+    blurBackground,
+    compressor,
+    longImage,
+    screenshot,
+    videoConvert,
+    convert,
+    viewer
+};
