b63f6e59fecf7e04177f1bb88a95ddd42ed997c4
talunte50 <talunte50@outlook.com>
2026-09-13 12:52:06 +0800

    chore: import src/i18n/es/nav.js

---
+++ src/i18n/es/nav.js
@@ -0,0 +1,46 @@
+export default {
+    editor: {
+        name: 'Editor',
+        title: 'Editor de Fotos',
+    },
+    beautifier: {
+        name: 'Embellecedor',
+        title: 'Embellecedor de Capturas de Pantalla',
+    },
+    rounded: {
+        name: 'Redondeado',
+        title: 'Foto a Redondeada',
+    },
+    remover: {
+        name: 'Removedor',
+        title: 'Removedor de Fondo de Foto',
+    },
+    blurBackground: {
+        name: 'Desenfoque',
+        title: 'Desenfocar fondo online',
+    },
+    compressor: {
+        name: 'Compresor',
+        title: 'Compresor de Imágenes',
+    },
+    longImage: {
+        name: 'Imagen larga',
+        title: 'Compositor de imagen larga',
+    },
+    videoConvert: {
+        name: 'Video',
+        title: 'Video Convert',
+    },
+    convert: {
+        name: 'Convert',
+        title: 'Image Convert'
+    },
+    viewer: {
+        name: 'Viewer',
+        title: 'Office Viewer'
+    },
+    screenshot: {
+        name: 'captura de pantalla',
+        title: 'tomar una captura de pantalla'
+    }
+};
