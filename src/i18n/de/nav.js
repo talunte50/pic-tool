7a0f4467af7cb7962db3b48b1f325605f8678a7c
talunte50 <talunte50@outlook.com>
2026-09-13 12:51:32 +0800

    chore: import src/i18n/de/nav.js

---
+++ src/i18n/de/nav.js
@@ -0,0 +1,46 @@
+export default {
+    editor: {
+        name: 'Editor',
+        title: 'Fotoeditor'
+    },
+    beautifier: {
+        name: 'Verschönern',
+        title: 'Screenshot verschönern'
+    },
+    rounded: {
+        name: 'Rounded',
+        title: 'Foto mit runden Ecken'
+    },
+    remover: {
+        name: 'Remover',
+        title: 'Foto-Hintergrund entfernen'
+    },
+    blurBackground: {
+        name: 'Weichz.',
+        title: 'Hintergrund online weichzeichnen',
+    },
+    compressor: {
+        name: 'Compressor',
+        title: 'Bildkompressor'
+    },
+    longImage: {
+        name: 'Langbild',
+        title: 'Langbild erstellen'
+    },
+    videoConvert: {
+        name: 'Video',
+        title: 'Videokonverter'
+    },
+    convert: {
+        name: 'Convert',
+        title: 'Bildkonverter'
+    },
+    viewer: {
+        name: 'Viewer',
+        title: 'Office Viewer'
+    },
+    screenshot: {
+        name: 'Screenshot',
+        title: 'Screenshot erstellen'
+    }
+}
